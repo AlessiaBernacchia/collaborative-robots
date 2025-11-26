d07504d4238f28a3d753fcc7edaa605dd1ceb155
Alessia Bernacchia <alessia.bernacchia@gmail.com>
2025-11-26 01:33:58 +0100

    add towers and bricks

--- main.py
+++ main.py
@@ -17,24 +17,33 @@ if __name__ == "__main__":
     # initialize bricks
     brick_A = Brick(sm.SE3(0.0, 0.5, 0.15))
     brick_B = Brick(sm.SE3(0.0, 0.5, 0.05))
-    brick_C = Brick(sm.SE3(0.1, 0.5, 0.15))
-    brick_D = Brick(sm.SE3(0.1, 0.5, 0.05))
+    brick_C = Brick(sm.SE3(0.11, 0.5, 0.15))
+    brick_D = Brick(sm.SE3(0.11, 0.5, 0.05))
 
-    bricks = [brick_A, brick_B, brick_C, brick_D]
+    brick_E = Brick(sm.SE3(0.22, 0.5, 0.15))
+    brick_F = Brick(sm.SE3(0.22, 0.5, 0.05))
+    brick_G = Brick(sm.SE3(-0.11, 0.5, 0.15))
+    brick_H = Brick(sm.SE3(-0.11, 0.5, 0.05))
+
+    bricks = [brick_A, brick_B, brick_C, brick_D, brick_E, brick_F, brick_G, brick_H]
 
     # add bricks to the environment
     for b in bricks:
         env.add(b.obj)
 
     # initialize the tower
-    base = sm.SE3(0.0, 0.0, 0.05)
-    tower = Tower(base_pose=base, max_height=4)
+    base_A = sm.SE3(0.2, 0.0, 0.05)
+    tower_A = Tower(base_pose=base_A, max_height=3)
     
+    base_B = sm.SE3(0.0, 0.0, 0.05)
+    tower_B = Tower(base_pose=base_B, max_height=4)
+    
+    towers = [tower_A, tower_B]
     # initialize the controller of the robot
     controller = Controller(env)       
 
     # initialize the sensor
-    sensor = Sensor(env, bricks=bricks, towers=[tower], robots=[panda_agent])
+    sensor = Sensor(env, bricks=bricks, towers=towers, robots=[panda_agent])
     
     # start simulation by iterating each brick in the list
     for _ in range(len(bricks)):
