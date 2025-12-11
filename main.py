dc44eb46e15c924fba336f108b9f0ac6808f58be
Alessia Bernacchia <alessia.bernacchia@gmail.com>
2025-12-11 23:20:22 +0100

    code to run fron guests to see only the simulation

--- main.py
+++ main.py
@@ -753,8 +753,8 @@ def main():
         main()
     elif task_name == 'exit':
         return 0
-    elif task(task_name, global_plot=True, all_plots=True, save=True):  # task ended successfully
-        main()
+    elif task(task_name, global_plot=False, all_plots=False, save=False):  # put True True to viz all plots
+        main()  # task ended successfully
     else: # task doesn't end successfully
         print("TASK NOT END AS EXPECTED")
         return
